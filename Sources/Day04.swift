41d94782aed23ed5c2ee29dac0d5a7f18664a58b
egrishina <egrishina22@fed.idserve.net>
2023-12-10 00:24:53 +0300

    Setup day 4

---
+++ Sources/Day04.swift
@@ -0,0 +1,25 @@
+//
+//  Day04.swift
+//  
+//
+//  Created by Ekaterina Grishina on 10/12/23.
+//
+
+import Foundation
+
+struct Day04: AdventDay {
+
+    var data: String
+
+    private var entities: [<#Data Type#>] {
+        <#Return prepared data#>
+    }
+
+    func part1() -> Any {
+        <#Return answer#>
+    }
+
+    func part2() -> Any {
+        <#Return answer#>
+    }
+}
